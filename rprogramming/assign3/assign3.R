ce54612c619d860269ec1ac10bdb98920189786c
Chandra Koduru <kscraja@gmail.com>
2014-06-01 12:55:33 +0530

    finished prog assignment 3

--- rprogramming/assign3/assign3.R
+++ rprogramming/assign3/assign3.R
@@ -94,3 +94,52 @@ rankhospital <- function(state, outcome, rank) {
 #print(rankhospital("TX", "heart failure", 4))
 #print(rankhospital("MD", "heart attack", "worst"))
 #print(rankhospital("MN", "heart attack", 5000))
+
+rankall <- function(outcome, rank) {
+  # lowest mortality column: 11 - Heart attack, 17 - Heart-failure, 23 - pneumonia 
+  if (!isValidOutcome(outcome)) {
+    stop("invalid outcome")
+  }
+  if (!isValidState(state)) {
+    stop("invalid state")
+  }
+  
+  mortalityOfOutcome <- data.frame(HospitalName = baseData$Hospital.Name, State=baseData$State, Mortality=as.numeric(baseData[[outcome]]))
+  #removing NA from the dataframe
+  mortalityOfOutcome <- mortalityOfOutcome[complete.cases(mortalityOfOutcome),]
+  
+  
+  #picking the state data
+  getHostpitalAtGivenRank <- function(stateMortality, rank) {
+    stateMortality$Order <- order(stateMortality$Mortality, stateMortality$HospitalName)
+    bestRank = 1
+    worstRank = nrow(stateMortality)
+    if (rank == "best") {
+      rank <- bestRank
+    } else if (rank == "worst") {
+      rank <- worstRank
+    }
+    
+    if (rank <  bestRank || rank > worstRank) {
+      as.character(NA)
+    } else {
+      hospitalAtGivenRank <- stateMortality[stateMortality$Order[rank], ]
+      r <- hospitalAtGivenRank$HospitalName
+      as.character(r)
+    }
+  }
+  #splitting the dataset by state 
+  s <- split(mortalityOfOutcome, mortalityOfOutcome$State)
+  allStateNames <- unique(mortalityOfOutcome$State)
+  
+  out <- data.frame(hospital=character(), state=character())
+  names(out) <- c("hospital", "state")
+  for(state in allStateNames) {
+    hospital <- getHostpitalAtGivenRank(s[[state]], rank)
+    out <- rbind(out, data.frame(hospital=hospital, state=state))
+  }
+  out
+}
+
+print(head(rankall("heart attack", 20), 10))
+print(head(rankall("pneumonia", "worst"), 3))
