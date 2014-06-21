8222e1b613002def92074ffbcca57883f72230df
Chandra Koduru <kscraja@gmail.com>
2014-06-21 11:40:38 +0530

    Added w2 notes of reprod_research

---
+++ reproducible_research/w2/w2_coding_standards.R
@@ -0,0 +1,6 @@
+# 
+# * text files
+# * indentation
+# * limit the width of the code (80 cols)
+# * limit the length of individual functions
+#     * split the functions into smaller and logical
---
+++ reproducible_research/w2/w2_markdow.R
@@ -0,0 +1,47 @@
+
+# italics
+
+*This text will be italicised*
+    
+# bold
+**bolded text!** 
+    
+# -> h1
+## -> h2
+### -> h3
+    
+- unordered list
+- ul 1
+- ul 2
+
+# any line begining with number will become ordered list
+1. ordered
+2. ordered 
+3. ordered
+
+# putting ext
+[text](http://url.com)
+
+#advanced link
+I spend so much time [R blogger][1]
+
+[1]: http://www.r-bloggers.com "R bloggers"
+
+# new lines
+first line
+second line  
+
+################# R Markdown ####################
+
+* R markdown is the integration of R code with markdown
+* Allows one to create documents containing "live" R code
+* R code is evaluated as part of the processing of the markdown
+* Results from R code are inserted into markdown content
+* A core tool in literate statistical programming
+* rmd is the extension
+* R markdown can be converted to standard markdown using the knitr package in R
+* markdown package in R
+* slidify package : knitr + markdown
+
+
+
